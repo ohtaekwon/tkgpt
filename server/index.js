34206fa2b2b9cc4d771caab8bde4e1d68e1bceba
ohtaekwon <ohtk1119@gmail.com>
2023-05-03 21:23:40 +0900

    Feat server run file

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import http from "http";
+import "dotenv/config";
+
+const { PORT, MONGO_DB_URL } = process.env;
+
+const app = express();
+app.use(cors);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// app.use("/api/v1");
+
+const port = PORT || 8000;
+const server = http.createServer(app);
+
+mongoose.set("strictQuery", false); // strict 모드 해제
+mongoose
+  .connect(MONGO_DB_URL)
+  .then(() => {
+    console.log("MongoDB connected");
+    server.listen(port, () =>
+      console.log(`Server is listening on port ${port}`)
+    );
+  })
+  .catch((error) => {
+    console.log(`This is Error : ${{ error }}`);
+    process.exit(1);
+  });
