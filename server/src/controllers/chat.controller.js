41e0623acf51181512e0b3eef7d5f91e723f047c
ohtaekwon <ohtk1119@gmail.com>
2023-05-03 21:27:39 +0900

    Feat Add chatGPT's controller

---
+++ server/src/controllers/chat.controller.js
@@ -0,0 +1,21 @@
+import chatGPT from "../config/chat.config.js";
+import responseHandler from "../handlers/response.handler.js";
+
+const chatCompletion = async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const answer = await chatGPT.openApi.createChatCompletion({
+      model: "text-davinci-003",
+      prompt: prompt,
+      temperature: 0,
+      max_tokens: 3000,
+    });
+
+    const text = answer.data.choices[0].text;
+
+    responseHandler.ok(res, { text });
+  } catch (error) {
+    responseHandler.error(res, error.message);
+  }
+};
