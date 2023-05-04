a0e60b5068da0f9338d1901f2ad47e330c59c566
ohtaekwon <ohtk1119@gmail.com>
2023-05-04 17:09:52 +0900

    Chore route connect

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,66 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import AuthRoute from "components/AuthRoute";
+import ProtectedRoute from "components/ProtectedRoute";
+import Home from "pages/Home";
+import SignIn from "pages/SignIn";
+import SignUp from "pages/SignUp";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { ToastContainer } from "react-toastify";
 
 function App() {
-  return <>dd</>;
+  const theme = createTheme({
+    palette: { mode: "dark" },
+  });
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        {/* mui reset css */}
+        <CssBaseline />
+        <ToastContainer
+          position="bottom-left"
+          autoClose={4000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnHover
+        />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/signin"
+              element={
+                <AuthRoute>
+                  <SignIn />
+                </AuthRoute>
+              }
+            />
+            <Route
+              path="/signup"
+              element={
+                <AuthRoute>
+                  <SignUp />
+                </AuthRoute>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
