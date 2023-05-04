f24eb6d778b6559016a4d21b3e318e80b007b915
ohtaekwon <ohtk1119@gmail.com>
2023-05-04 17:07:37 +0900

    Feat ProtectedPage component

---
+++ client/src/components/ProtectedRoute.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Box } from "@mui/material";
+import userApis from "lib/api/modules/user.api";
+import Loading from "./Loading";
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const navigation = useNavigate();
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+
+  React.useEffect(() => {
+    const checkToken = async () => {
+      setIsLoading(true);
+      const { response, error } = await userApis.checkToken();
+
+      if (error) {
+        localStorage.removeItem("tkgpt");
+        navigation("/signin");
+      }
+
+      if (response) {
+        localStorage.setItem("username", response.username);
+        setIsLoading(false);
+      }
+    };
+
+    const token = localStorage.getItem("tkgpt");
+
+    if (token) checkToken();
+    else navigation("/signin");
+  }, [navigation]);
+
+  return (
+    <>
+      {isLoading && <Loading />}
+      {!isLoading && <Box sx={{ height: "100vh" }}>{children}</Box>}
+    </>
+  );
+};
+
+export default ProtectedRoute;
